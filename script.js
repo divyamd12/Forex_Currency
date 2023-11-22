bf7e8a1f59bb73e75b42f12d93a6451afefc5f14
Khilee01 <khileesinghal@gmail.com>
2023-11-22 23:24:46 +0530

    few changes made extra files deleted

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-    $('#dateRangePicker').daterangepicker({
-        opens: 'left', // Adjust the calendar's position
-        locale: {
-            format: 'YYYY-MM-DD'
-        }
-    }, function(start, end, label) {
-        // Handle the selected date range, if needed
-        console.log("Selected date range: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    });
-});
+
